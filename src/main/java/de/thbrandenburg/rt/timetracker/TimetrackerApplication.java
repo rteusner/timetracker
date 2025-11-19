ba85b286da435312958edd626926bd9cf7b17447
Ralf Teusner <teusner@th-brandenburg.de>
2025-11-19 10:45:21 +0100

    remove vaadin

--- src/main/java/de/thbrandenburg/rt/timetracker/TimetrackerApplication.java
+++ src/main/java/de/thbrandenburg/rt/timetracker/TimetrackerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TimetrackerApplication implements AppShellConfigurator {
+public class TimetrackerApplication {
 
 
     public static void main(String[] args) {
